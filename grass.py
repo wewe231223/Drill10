a6215752cd019a9063451a4ba18030d8c56f7b50
wewe231223 <wewe231223@kpu.ac.kr>
2023-10-18 18:43:09 +0900

    잔디밭을 초기화할때 출력할 위치 입력 받게 설정

--- grass.py
+++ grass.py
@@ -2,11 +2,14 @@ from pico2d import load_image
 
 
 class Grass:
-    def __init__(self):
+    def __init__(self, x: int, y: int):
         self.image = load_image('grass.png')
+        self.x = x
+        self.y = y
+
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(self.x, self.y)
 
     def update(self):
         pass
